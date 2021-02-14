f6289584427d3a28251d628a45ef41ed94beb2f5
AbndreyMichaylov <48345409+AbndreyMichaylov@users.noreply.github.com>
2021-02-15 01:05:03 +0300

    добился овтетов на сообщения и загрузку пришедших изображений

--- env/script.py
+++ env/script.py
@@ -2,12 +2,27 @@ import pytesseract
 from PIL import Image
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.utils import get_random_id
+import requests
+import uuid
 
 session = vk_api.VkApi(token='88c94357228fe11170d280783a866ff40d226334bcac34cf5edc8f2563c9aa1fb0f9df566b971f3813cfb')
 longpool =  VkBotLongPoll(vk=session, group_id=202597974, wait=1)
+vk = session.get_api()
 
-for eventt in longpool.listen():
-    if eventt.type == VkBotEventType.MESSAGE_NEW:
+for event in longpool.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        #for i in event.object:
+            #print(i)
+        #exit()
+        image_url = event.object["attachments"][0]["photo"]["sizes"][-1]["url"]
+        image = requests.get(image_url)
+        random_name = str(uuid.uuid4()) + '.jpg'
+        with open('C:\\Users\\cjcbc\Desktop\\projects\\bot_image_translater\\env\\not_translated_images\\' + random_name , 'wb') as f:
+            f.write(image.content)
+        vk.messages.send(user_id=event.object["from_id"],
+                         random_id=get_random_id(),
+                         message='kek')
         print('FUCK')
 
 exit()
